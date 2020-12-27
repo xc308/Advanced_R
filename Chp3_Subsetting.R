1c7be37cda0c6a4fa6c8b8df01f2cc66bf5e9dbd
xc308 <xc308@exeter.ac.uk>
2020-12-27 21:47:22 +0100

    add subsetting rules of lists and matrices

--- Chp3_Subsetting.R
+++ Chp3_Subsetting.R
@@ -66,5 +66,89 @@ y[c("a", "d")]
 
 
 
+#--------------#
+# 3.1.2 Lists
+#--------------#
+
+# Subsetting a list works the same as subsetting an atomic vector
+# use [ ] will return a list
+# use [[ ]] or $ will pull out the component of a list
+
+L <- list(1:3, c(T, F), c("a", "b"))
+str(L)
+L[1]
+# return the 1st list
+# [[1]]
+# [1] 1 2 3
+L[2]
+# [[1]]
+# [1]  TRUE FALSE
+
+
+# to pull out the component of the 2nd list
+L[[2]]
+# [1]  TRUE FALSE
+
+
+#--------------------------#
+# 3.1.3 Matrices and Arrays
+#--------------------------#
+
+# 3 ways to subset high-dim structures
+  # multiple vectors
+  # a single vectors
+  # a matrix
+
+
+## subset with a multiple vector
+a <- matrix(1:9, nrow = 3)
+colnames(a) <- c("A", "B", "C")
+a
+a[1:2, ]
+
+a[c(T, F, T), c("B", "A")]
+
+a[0, -2]
+#  A C
+
+
+## subset with a single vector
+# because matrix / arrays are implemented as vectors
+# so, it's possilbe to subset them with a single vector
+# In this case, matrix / arrays behave like vectors
+
+# arrays in R are stored in column-major order
+
+vals <- outer(1:5, 1:5, FUN = "paste", sep = "*.*")
+vals
+# in column-major order
+# matrix behave like a vector
+vals[c(4, 15)] # subset with a single vector
+# pull out the element of the matrix (like vector)
+# at the postion of 4th and 15th
+
+# "4*.*1" "5*.*3"
+
+
+## Subset using a matrix
+sel <- matrix(ncol = 2, byrow = TRUE, 
+              c(1, 1,
+                3, 1,
+                2, 4))
+
+# each row of the matrix specified the location
+# of the value, e.g (1, 1) the 1st row, the 1st col,
+# the number of cols of matrix corresponds to the dimension of arry being subsetted
+
+sel
+vals[sel]
+# [1] "1*.*1" "3*.*1" "2*.*4"
+# the result is a vector
+
+# the n# of cols of the selcting matrix corresponds to the dim of the array being subsetted
+# so if it's 2, then subset a matrix, 
+# if it's 3, then subset a 3-d array
+
+
 
 

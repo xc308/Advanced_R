633ddaa7aa136daac120ea83945fcf6eb4eb9232
xc308 <xc308@exeter.ac.uk>
2022-12-22 23:11:19 +0000

    sapply & vapply

--- Chp11_Functionals_2022.R
+++ Chp11_Functionals_2022.R
@@ -137,6 +137,152 @@ for (i in seq_along(xs)) {
 
 
 
+##============================================
+# 11.2 For loop functional: friends of lapply()
+##============================================
+
+# sapply() vapply(): 
+  # variants of lapply() that produces
+  # vectors, matrices, arrays as output, instead of list
+
+# Map() and mapply():
+  # iterates over multiple input data structures in parallel
+
+# mclapply() and mcMap() :
+  # multi-core
+  # so parallel version of lapply() and Map()
+
+# rollapply():
+  # write a new function to solve a new problem
+
+
+#----------------------------------------
+# 11.2.1 Vector output:sapply and vapply
+#----------------------------------------
+
+# simplify output of lapply to atomic vector
+
+# sapply():
+  # good for interactive use as saving typing
+  # not good for inside of a function 
+    # will get errors if the supply the wrong input 
+
+# vapply():
+  # need an arg to specify the output type
+  # gives more information on error
+  # never silent failure
+  # better suited for inside other functions
+
+# when given a data frame, 
+  # sapply() and vapply() gives the same result
+
+# when given an empty list
+  # sapply() returns an empty list 
+    # not the more correct one a zero length logical vector
+
+sapply(mtcars, is.numeric)
+# mpg  cyl disp   hp drat   wt qsec   vs 
+#TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE 
+#am gear carb 
+#TRUE TRUE TRUE
+
+length(logical(1)) 
+# [1] 1
+# If length(FUN.VALUE) == 1 a vector of the same length as X is returned, 
+# otherwise an array. 
+
+#vapply(X, FUN, FUN.VALUE)
+vapply(mtcars, is.numeric, 1)
+vapply(mtcars, is.numeric, logical(1))
+
+length(mtcars) # 11
+str(mtcars)
+# 'data.frame':	32 obs. of  11 variables:
+
+logical(0) # logical(0)
+logical(1) # [1] FALSE
+logical(2) # [1] FALSE FALSE
+logical(3) # [1] FALSE FALSE FALSE
+
+
+sapply(list(), is.numeric)
+# list()
+vapply(list(), is.numeric, logical(1))
+# logical(0)
+
+
+## if a function returns results of different types or lenths
+  # sapply() silently return a list
+  # vapply() throw an error
+
+
+# Example: extracting the class of col in a data frame
+  # if falsely assume that class only has one value
+  # and thus use sapply()
+  # won't find out the problem until some future function 
+    # is given a list instead of a character vector
+
+df <- data.frame(x = 1:10, y = letters[1:10])
+# two variables one is x the other is y
+
+sapply(df, class)
+#         x           y 
+#   "integer" "character" 
+vapply(df, class, character(1))
+#         x           y 
+#   "integer" "character" 
+
+df2 <- data.frame(x = 1:10, y = Sys.time() + 1:10)
+
+sapply(df2, class)
+# $x
+#[1] "integer"
+
+#$y
+#[1] "POSIXct" "POSIXt"
+
+vapply(df2, class, character(1))
+# Error in vapply(df2, class, character(1)) : values must be length 1,
+# but FUN(X[[2]]) result is length 2
+
+# > character(1)
+#[1] ""
+#> character(2)
+#[1] "" ""
+
+
+## sapply():
+  # a thin wrapper around lapply() that transform the
+    # list output into a vector
+
+## vapply():
+  # an implementation of lapply() that assigns results
+    # to a vector or a matrix of appropriate type instead of a list
+
+
+function(x, f, f.value, ...) {
+  out <- matrix(rep(f.value, length(x)), nrow = length(x))
+  for (i in seq_along(x)) {
+  res <- f(x[i], ...)
+  stopifnot(
+    length(res) == length(f.value),
+    typeof(res) == typeof(f.value)
+  )
+  out[i, ] <- res
+  }
+  out
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 

a3debcc67babb0f263d3f26742bc98c3133c72d8
xc308 <xc308@exeter.ac.uk>
2023-01-19 23:10:11 +0000

    why R is slow

--- Chp16_Performance.R
+++ Chp16_Performance.R
@@ -83,6 +83,117 @@ microbenchmark(
 
 
 
+#--------------------------------
+# 16.4 Implementing performance
+#--------------------------------
+
+.subset2()
+# Internal Objects in Package base
+# from which to extract elements.
+# are essentially equivalent to the [ and [[ operators, 
+# except that methods dispatch does not take place.
+# to avoid expensive unclassing when applying the default method to an object
+# should not normally be invoked by end users.
+
+microbenchmark(
+  "[32, 11]" = mtcars[32, 11],
+  ".subset2" = .subset2(mtcars, 11)[32]
+)
+
+#     expr  min   lq    mean median     uq   max
+# [32, 11] 7334 7459 8296.50   7584 7729.5 66709
+# .subset2   84  126  164.12    167  168.0  1126
+
+
+#-------------------------------
+# 16.4.2 ifelse(), pmin(), pmax()
+#-------------------------------
+
+# squish() a function ensures the 
+  # smallest value in a vector is at least a
+  # the largest value is at most b
+
+squish_ife <- function(x, a, b) {
+  ifelse(x <= a, a, ifelse(x >= b, b, x))
+}
+# slow as ifelse is slow
+  # very general needs to evaluate all arg fully
+
+squish_p <- function(x, a, b) {
+  pmin(pmax(x, b), a)
+}
+# still slow because they take any number of elements 
+# and need to do complicated checks and determine 
+  # which method to use
+
+squish_in_place <- function(x, a, b) {
+  x[x <= a] <- a
+  x[x >= b] <- b
+  x
+}
+
+x <- runif(100, -1.5, 1.5)
+microbenchmark(
+  squish_ife = squish_ife(x, -1, 1), 
+  squish_p = squish_p(x, -1, 1),
+  squish_in_place = squish_in_place(x, -1, 1),
+  unit = "us"
+)
+# Unit: microseconds
+#   expr    min      lq     mean  median
+#squish_ife 16.167 17.3545 36.71904 17.9180
+#squish_p  9.168 10.5635 21.52105 11.3965
+#squish_in_place  1.834  2.3965  2.90901  2.7505
+
+## using subset is 10 times faster than pmin
+# and 18 times faster than ifeles
+
+# can even faster using C++
+
+
+# include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericVector squish_cpp(NumericVector x, double a, double b) {
+  int n = x.length();
+  NumericVector out(n);
+  
+  for (int i = 0; i < n; ++i) {
+    double xi = x[i];
+    if (xi < a) {
+      out[i] = a;
+    } else if (xi > b) {
+      out[i] = b;
+    } else {
+      out[i] = xi;
+    }
+  }
+  
+  return out;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

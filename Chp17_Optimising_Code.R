0b89245128effd9bfce969f186c9352ff735065d
xc308 <xc308@exeter.ac.uk>
2023-01-23 23:26:35 +0000

    vectorisation

--- Chp17_Optimising_Code.R
+++ Chp17_Optimising_Code.R
@@ -67,6 +67,70 @@ microbenchmark(
 
 
 
+#---------------
+# 17.6 Vectorise
+#---------------
+
+# think entire vectors instead of components of a vector
+# the loops in a vectorised function are written in C instead of R
+# mush faster
+
+# using vectorisation for performance means
+# finding the existing R function that's implemented in C
+# and mostly closely applies to your problem
+
+# rowSums(), colSums(), rowMeans(), colMeans()
+# vectorised matrix function will always be faster tha apply()
+
+# vectorised subsetting can lead to big improvements in speed
+
+
+# matrix algebra is a general example of vectorisation
+
+
+# downside of vectorisation is harder to predict how operation will scale
+
+# expect: looking up 10 elements will take 10* as long as looking up 1
+# but it only takes 9 * longer to look up for 100 elemtens 
+# than it does to look up for 1
+
+lookup <- setNames(as.list(sample(100, 26)), letters)
+
+x1 <- "j"
+x10 <- sample(letters, 10)
+x100 <- sample(letters, 100, replace = T)
+
+microbenchmark(
+  lookup[x1],
+  lookup[x10],
+  lookup[x100]
+)
+lookup["j"]
+# $j
+# [1] 98
+lookup[["j"]]
+# [1] 98
+
+#Unit: nanoseconds
+#expr  min   lq    mean median     uq   max
+#lookup[x1]  209  251  269.89  251.0  271.5   918
+#lookup[x10]  876  917 1004.84  918.0  959.0  5125
+#lookup[x100] 2876 3501 4609.83 3875.5 5042.0 16084
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
